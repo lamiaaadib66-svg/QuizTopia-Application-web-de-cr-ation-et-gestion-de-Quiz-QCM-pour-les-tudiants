ceae6ecb958f24ac93227dff54cdbfe1e238b6e5
MHamed <aitsidiouali@gmail.com>
2025-12-10 13:18:28 +0100

    Created all the controller skeletons

---
+++ backend/app/Http/Controllers/SessionQuizController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Session_Quiz;
+use Illuminate\Http\Request;
+
+class SessionQuizController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Session_Quiz $session_Quiz)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Session_Quiz $session_Quiz)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Session_Quiz $session_Quiz)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Session_Quiz $session_Quiz)
+    {
+        //
+    }
+}
